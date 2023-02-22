efed81bf3d977cd10a8984e40973690ab14be545
chanukag <chanukag@zone24x7.com>
2023-02-23 00:17:33 +0530

    styles

--- src/features/todo/Todo.tsx
+++ src/features/todo/Todo.tsx
@@ -1,51 +1,99 @@
 import React, {useEffect, useState} from 'react';
-import {Button, ScrollView, Switch, Text, TextInput, View} from 'react-native';
+import {Button, ScrollView, StyleSheet, Switch, Text, TextInput, View,} from 'react-native';
 
-import {useLazyGetToDoQuery, useLazyGetToDosQuery, useCreateToDoMutation, useUpdateToDoMutation, useDeleteToDoMutation} from '../../api/todoApiSlice';
+import {
+    useCreateToDoMutation,
+    useDeleteToDoMutation,
+    useLazyGetToDosQuery,
+    useUpdateToDoMutation,
+} from '../../api/todoApiSlice';
 
 export function Todo() {
-
-    const [triggerGetToDosQuery, { data: toDos}] = useLazyGetToDosQuery();
+    const [triggerGetToDosQuery, {data: toDos}] = useLazyGetToDosQuery();
     // const [triggerGetToDoQuery, { data: mydata }] = useLazyGetToDoQuery();
-    const [triggerCreateToDoMutation, {data, isLoading, error, isError}] = useCreateToDoMutation();
+    const [triggerCreateToDoMutation, {data, isLoading, error, isError}] =
+        useCreateToDoMutation();
     const [triggerUpdateToDoMutation] = useUpdateToDoMutation();
     const [triggerDeleteToDoMutation] = useDeleteToDoMutation();
     const [todo, setTodo] = useState('');
 
-    useEffect(()=>{
-      triggerGetToDosQuery().unwrap()
-    },[])
+    useEffect(() => {
+        triggerGetToDosQuery().unwrap();
+    }, []);
 
     return (
-        <View style={{ flexGrow: 1 }}>
-            <Text style={{textAlign: 'center',  fontSize: 20, margin: 20 }}>ToDo API Slice</Text>
-            <View style={{ flexDirection: 'row' }}>
-            <TextInput
-                style={{ flexGrow: 1, textAlign:'center', fontSize: 20, borderStyle:'solid', borderWidth: 1, borderRadius: 2,  borderColor: '#CCCCCC'}}
-                aria-label="Set increment amount"
-                onChangeText={(value: string) => {
-                    setTodo(value)
-                }}
-            />
-            <Button title="Add +" onPress={()=>triggerCreateToDoMutation({title: `${todo}`, isDone: false})}/>
+        <View style={styles.textinputStyle}>
+            <Text style={styles.toDoHeaderStyle}>ToDo API Slice</Text>
+            <View style={{flexDirection: 'row'}}>
+                <TextInput
+                    style={[styles.textinputStyle]}
+                    aria-label="Set increment amount"
+                    onChangeText={(value: string) => {
+                        setTodo(value);
+                    }}
+                />
+                <Button
+                    title="Add +"
+                    onPress={() =>
+                        triggerCreateToDoMutation({title: `${todo}`, isDone: false})
+                    }
+                />
             </View>
-            <ScrollView contentContainerStyle={{flexGrow: 1, marginHorizontal: 50, marginVertical: 20}}>
-                {toDos && toDos.map((todo: any) => (
-                    <View key={todo.id} style={{ flexDirection: 'row', flexGrow: 1}}>
-                        <Text style={{marginBottom: 30, fontSize: 20, flexGrow: 1}}>{todo.title}</Text>
+            <ScrollView contentContainerStyle={styles.scrollViewStyle}>
+                {toDos &&
+                toDos.map((todo: any) => (
+                    <View key={todo.id} style={{flexDirection: 'row', alignContent: 'center', marginBottom: 10}}>
+                        <Text style={styles.toDoAddTxtViewStyle}>{todo.title}</Text>
                         <Switch
                             key={todo.id}
-                            style={{ alignContent: 'flex-end'}}
-                            onValueChange={()=>{
-                                triggerUpdateToDoMutation({...todo,isDone: !todo.isDone });
+                            style={styles.switchStyle}
+                            onValueChange={() => {
+                                triggerUpdateToDoMutation({...todo, isDone: !todo.isDone});
                                 todo.isDone = !todo.isDone;
                             }}
-                            value={!todo.isDone}
+                            value={todo.isDone}
                         />
-                        <Button title="Delete" onPress={()=>triggerDeleteToDoMutation(todo.id)}/>
+                        <Button
+                            title="Delete"
+                            onPress={() => triggerDeleteToDoMutation(todo.id)}
+                        />
                     </View>
                 ))}
             </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    textinputStyle: {
+        flexGrow: 1,
+        textAlign: 'center',
+        fontSize: 21,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 2,
+        marginHorizontal: 20,
+        borderColor: 'green',
+    },
+    toDoAddTxtViewStyle: {
+        marginBottom: 30,
+        fontSize: 20,
+        width: 160,
+        alignSelf: 'flex-start',
+        textAlign: 'left',
+    },
+    scrollViewStyle: {
+        flexGrow: 1,
+        marginHorizontal: 20,
+        marginVertical: 20,
+    },
+    switchStyle: {
+        alignContent: 'flex-end',
+        marginRight: 40,
+    },
+    toDoHeaderStyle: {
+        textAlign: 'center',
+        fontSize: 20,
+        margin: 20,
+    },
+});
